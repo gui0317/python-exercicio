7edc3dd4910f11b33fce1ebb2f84ae4244814222
gui0317 <guilherme.castroa17@gmail.com>
2024-09-15 13:44:38 -0300

    Add files via upload

---
+++ exercicio.py
@@ -0,0 +1,16 @@
+nome= input('insira seu nome:')
+idade= int(input('insira sua idade:'))
+altura=float(input('insira sua altura:'))
+programador=input ('voce é programador?:')
+if programador.lower()== 'sim':
+    programador=True 
+    print('parabens')
+else:
+    programador=False 
+    print('comece')
+numero=float(input('insira um numero:'))
+numero2=float(input('insira um numero:'))
+soma= numero+numero2
+subtraçao= numero-numero2
+mutiplicaçao= numero*numero2
+divisão= numero/numero2
